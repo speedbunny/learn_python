d193b22855d28ca29ec910ddddb4286dc90f9b0e
Sarah Eaglesfield <speedbunny@gmail.com>
2022-07-01 19:55:31 +0100

    Update learn_lists.py

--- arrays/learn_lists.py
+++ arrays/learn_lists.py
@@ -1,1 +1,27 @@
-x
+#Create a List of Numbers
+my_list = []
+for x in range(1,10):
+my_list.append(x)
+
+#One-shot List
+my_list = [x for x in range(1,10)]
+
+#List constructor
+my_list = list(("apple", "banana", "cherry"))
+
+#Elements are referenced by numbers
+first_ele = my_list[0]
+last_ele = my_list[-1]
+
+#Operators for statistics
+how_many_items = len(my_list)
+biggest_val = max(my_list)
+smallest_val = min(my_list)
+avg_val = sum(my_list) / len(my_list)
+
+#List Datatypes
+list1 = ["apple", "banana", "cherry"] #string
+list2 = [1, 5, 7, 9, 3] #numeric
+list3 = [True, False, False] #bool
+list4 = ["abc", 34, True, 40, "male"] #mixed
+print(type(my_list)) #What Datatype is it?
