cd5c0dbbb91f76a1a128cbeb5052ba4085a89ba4
Sarah Eaglesfield <speedbunny@gmail.com>
2022-07-23 17:21:19 +0100

    Create learn_sets.py

---
+++ arrays/learn_sets.py
@@ -0,0 +1,21 @@
+#Create a set
+thisset = {"apple", "banana", "cherry", "apple"}
+print(thisset)
+
+#Using set constructor
+thisset = set(("apple", "banana", "cherry")) 
+
+#Sets can be any datatype
+set1 = {"apple", "banana", "cherry"}
+set2 = {1, 5, 7, 9, 3}
+set3 = {True, False, False} 
+
+#Sets can be a mixed datatype
+set1 = {"abc", 34, True, 40, "male"} 
+
+#Set datatype is "Set"
+myset = {"apple", "banana", "cherry"}
+print(type(myset))
+
+#Set items are unordered, unchangeable, and do not allow duplicate values.
+#Once a set is created, you cannot change its items, but you can remove items and add new items.
