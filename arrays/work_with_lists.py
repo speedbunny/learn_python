8f55c430755e7d3a1cc4b5d1a7804c1c952f4f98
Sarah Eaglesfield <speedbunny@gmail.com>
2022-07-23 19:35:39 +0100

    Update work_with_lists.py

--- arrays/work_with_lists.py
+++ arrays/work_with_lists.py
@@ -4,9 +4,11 @@ print(thislist[1])  #banana
 print(thislist[2:5]) #3rd 4th 5th item
 print(thislist[:4]) #Leaving out 0 the range starts at first item
 print(thislist[2:]) #All items from cherry to end
+
 #Negative
 print(thislist[-1]) #last item of list
 print(thislist[-4:-1]) #cherry to kiwi
+
 #Is an item in the list?
 if "apple" in thislist:
   print("Yes, 'apple' is in the fruits list") 
@@ -45,7 +47,7 @@ del thislist
 
 #Remove all items in list with clear
 thislist.clear()
-#Change List Items
+#Replace List Items
 
 #Replace one value
 thislist[1] = "blackcurrant"
@@ -65,6 +67,22 @@ thislist = ["apple", "banana", "cherry"]
 thislist[1:3] = ["watermelon"]
 print(thislist) 
 
+#Joining lists
+list1 = ["a", "b", "c"]
+list2 = [1, 2, 3]
+
+list3 = list1 + list2
+print(list3) 
+
+#Join with append
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+
+for x in list2:
+  list1.append(x)
+
+print(list1) 
+
 #Extend a list with another list
 thislist = ["apple", "banana", "cherry"]
 tropical = ["mango", "pineapple", "papaya"]
@@ -76,3 +94,55 @@ thislist = ["apple", "banana", "cherry"]
 thistuple = ("kiwi", "orange")
 thislist.extend(thistuple)
 print(thislist)
+
+#Copy a List with copy
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist.copy()
+print(mylist)
+
+#Copy a List with list
+thislist = ["apple", "banana", "cherry"]
+mylist = list(thislist)
+print(mylist)
+
+#Copy list with comprehension
+mylist = [x for x in fruits] 
+
+#Sort a list ascending alphanumerically (case sensitive)
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort()
+print(thislist)
+
+#Sort a list ascending alphanumerically (case insensitive)
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.sort(key = str.lower)
+print(thislist)
+
+#Sort Descending
+thislist = [100, 50, 65, 82, 23]
+thislist.sort(reverse = True)
+print(thislist)
+
+#Reverse list with no sort
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.reverse()
+print(thislist)
+
+#Custom Sort
+def myfunc(n):
+  return abs(n - 50) #Closest to 50
+
+thislist = [100, 50, 65, 82, 23]
+thislist.sort(key = myfunc)
+print(thislist)
+
+#List comprehension
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = [x for x in fruits if "a" in x]
+print(newlist) 
+
+#newlist = [expression for item in iterable if condition == True]
+newlist = [x for x in fruits if x != "apple"] 
+
+
+
