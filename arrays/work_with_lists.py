f82ea3d2c46d69caf547b2444848484082788d9d
Sarah Eaglesfield <speedbunny@gmail.com>
2022-07-23 18:47:20 +0100

    Update work_with_lists.py

--- arrays/work_with_lists.py
+++ arrays/work_with_lists.py
@@ -10,3 +10,69 @@ print(thislist[-4:-1]) #cherry to kiwi
 #Is an item in the list?
 if "apple" in thislist:
   print("Yes, 'apple' is in the fruits list") 
+
+#Add List Item
+#Append a value at the end
+thislist = ["apple", "banana", "cherry"]
+thislist.append("orange")
+print(thislist)
+
+#Insert a value at specified index
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(2, "watermelon")
+print(thislist)
+
+#Remove List Item
+thislist = ["apple", "banana", "cherry"]
+thislist.remove("banana")
+print(thislist)
+
+#Remove By Index with pop
+thislist = ["apple", "banana", "cherry"]
+thislist.pop(1)
+print(thislist)
+
+#Pop without index removes last item.
+thislist.pop()
+
+#Remove by Index with del
+thislist = ["apple", "banana", "cherry"]
+del thislist[0]
+print(thislist)
+
+#Del can also delete whole list
+del thislist 
+
+#Remove all items in list with clear
+thislist.clear()
+#Change List Items
+
+#Replace one value
+thislist[1] = "blackcurrant"
+print(thislist)
+
+#Replace two values
+thislist[1:3] = ["blackcurrant", "watermelon"]
+print(thislist)
+
+#Replace one value with two values
+thislist = ["apple", "banana", "cherry"]
+thislist[1:2] = ["blackcurrant", "watermelon"]
+print(thislist) 
+
+#Replace two values with one value
+thislist = ["apple", "banana", "cherry"]
+thislist[1:3] = ["watermelon"]
+print(thislist) 
+
+#Extend a list with another list
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+thislist.extend(tropical)
+print(thislist)
+
+#Extend a list with any iterable object (tuples, sets, dictionaries etc.).
+thislist = ["apple", "banana", "cherry"]
+thistuple = ("kiwi", "orange")
+thislist.extend(thistuple)
+print(thislist)
