d3dcbe863f2623a28b3ba7c59d3062a5974f726e
Sarah Eaglesfield <speedbunny@gmail.com>
2022-07-23 18:05:57 +0100

    Update work_with_lists.py

--- arrays/work_with_lists.py
+++ arrays/work_with_lists.py
@@ -1,1 +1,12 @@
-
+#Access List Items
+thislist = ["apple", "banana", "cherry", "mango", "kiwi", "orange"]
+print(thislist[1])  #banana
+print(thislist[2:5]) #3rd 4th 5th item
+print(thislist[:4]) #Leaving out 0 the range starts at first item
+print(thislist[2:]) #All items from cherry to end
+#Negative
+print(thislist[-1]) #last item of list
+print(thislist[-4:-1]) #cherry to kiwi
+#Is an item in the list?
+if "apple" in thislist:
+  print("Yes, 'apple' is in the fruits list") 
